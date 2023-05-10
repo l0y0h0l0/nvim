816581f4a467e61a91ed625122176a9d6e93537a
Augustana <l0y0h0l0@163.com>
2023-05-10 23:02:11 +0800

    refactor: update dap

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -15,6 +15,68 @@ vim.keymap.set({ "i", "n", "v" }, "<F9>", "<cmd>lua require'dap'.toggle_breakpoi
 
 -- dap ui
 dapui.setup()
+
+local dap_breakpoint_color = {
+  breakpoint = {
+    ctermbg = 0,
+    fg = '#993939',
+    bg = '#31353f',
+  },
+  logpoing = {
+    ctermbg = 0,
+    fg = '#61afef',
+    bg = '#31353f',
+  },
+  stopped = {
+    ctermbg = 0,
+    fg = '#98c379',
+    bg = '#31353f'
+  },
+}
+
+vim.api.nvim_set_hl(0, 'DapBreakpoint', dap_breakpoint_color.breakpoint)
+vim.api.nvim_set_hl(0, 'DapLogPoint', dap_breakpoint_color.logpoing)
+vim.api.nvim_set_hl(0, 'DapStopped', dap_breakpoint_color.stopped)
+
+local dap_breakpoint = {
+  error = {
+    text = "",
+    texthl = "DapBreakpoint",
+    linehl = "DapBreakpoint",
+    numhl = "DapBreakpoint",
+  },
+  condition = {
+    text = 'ﳁ',
+    texthl = 'DapBreakpoint',
+    linehl = 'DapBreakpoint',
+    numhl = 'DapBreakpoint',
+  },
+  rejected = {
+    text = "",
+    texthl = "DapBreakpint",
+    linehl = "DapBreakpoint",
+    numhl = "DapBreakpoint",
+  },
+  logpoint = {
+    text = '',
+    texthl = 'DapLogPoint',
+    linehl = 'DapLogPoint',
+    numhl = 'DapLogPoint',
+  },
+  stopped = {
+    text = '',
+    texthl = 'DapStopped',
+    linehl = 'DapStopped',
+    numhl = 'DapStopped',
+  },
+}
+
+vim.fn.sign_define('DapBreakpoint', dap_breakpoint.error)
+vim.fn.sign_define('DapBreakpointCondition', dap_breakpoint.condition)
+vim.fn.sign_define('DapBreakpointRejected', dap_breakpoint.rejected)
+vim.fn.sign_define('DapLogPoint', dap_breakpoint.logpoint)
+vim.fn.sign_define('DapStopped', dap_breakpoint.stopped)
+
 -- event callback
 dap.listeners.after.event_initialized["dapui_config"] = function()
   dapui.open({})
@@ -55,7 +117,7 @@ require("nvim-dap-virtual-text").setup {
   -- e.g. 80 to position at column 80, see `:h nvim_buf_set_extmark()`
 }
 
--- language
+-- dap adapters configurations
 -- cpp
 dap.adapters.cppdbg = {
   id = "cppdbg",
--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,5 +1,5 @@
 require ('lualine').setup({
   options = {
-    theme = 'one_monokai'
+    theme = 'one_monokai',
   }
 })
