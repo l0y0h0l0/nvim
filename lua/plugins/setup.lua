770740c674f701c7e6c2559d417e110d43bddf0f
Augustana <l0y0h0l0@163.com>
2023-10-02 16:25:40 +0800

    feat(plugin): add comment

--- lua/plugins/setup.lua
+++ lua/plugins/setup.lua
@@ -101,6 +101,13 @@ return require('packer').startup(function(use)
   }
   use 'theHamsta/nvim-dap-virtual-text'
 
+  use {
+    'numToStr/Comment.nvim',
+    config = function()
+      require('Comment').setup()
+    end
+  }
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if packer_bootstrap then
