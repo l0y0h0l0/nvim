2e66df9da07cb557bfdee5f9d18d2427456473ae
Augustana <l0y0h0l0@163.com>
2024-01-04 22:31:44 +0800

    feat(keymap): custom function

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,6 +1,7 @@
 vim.g.mapleader = ';'
 
 local keymap = vim.keymap
+local opt = vim.opt
 
 ---------- plugins ----------
 -- nvim tree
@@ -12,3 +13,14 @@ keymap.set('n', '<c-h>', '<cmd>bprevious<cr>')
 keymap.set({'n'}, '<leader>q', '<cmd>q!<cr>')
 keymap.set({'n'}, '<leader>s', '<cmd>w<cr>')
 keymap.set({'n'}, '<leader>wq', '<cmd>wq<cr>')
+
+---------- custom ----------
+function _G.triggerWrap()
+  if opt.wrap:get() then
+    vim.cmd('set nowrap')
+  else
+    vim.cmd('set wrap')
+  end
+end
+
+keymap.set({'n'}, '<leader>w', '<cmd>lua triggerWrap()<cr>')
