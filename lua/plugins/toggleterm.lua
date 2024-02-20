f26e2c690bbc565151558e8e314627dee8c3fd57
Augustana <l0y0h0l0@163.com>
2024-02-20 21:47:12 +0800

    refactor(toggleterm): update toggleterm config

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -13,7 +13,7 @@ require('toggleterm').setup{
   shade_terminals = true, -- NOTE: this option takes priority over highlights specified so if you specify Normal highlights you should set this to false
   shading_factor = -30, -- the percentage by which to lighten terminal background, default: -30 (gets multiplied by -3 if background is light)
   start_in_insert = true,
-  insert_mappings = true, -- whether or not the open mapping applies in insert mode
+  insert_mappings = false, -- whether or not the open mapping applies in insert mode
   terminal_mappings = true, -- whether or not the open mapping applies in the opened terminals
   persist_size = false,
   persist_mode = false, -- if set to true (default) the previous terminal mode will be remembereddirection
